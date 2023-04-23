1fb8ad22e13fe681b9bcde2d922440707781b910
aries <ariesllesis050382@gmail.com>
2023-04-23 16:41:14 +0800

    e

--- resources/js/Pages/AdminDashboard/AdminPages/UserManagement/UserAdd.vue
+++ resources/js/Pages/AdminDashboard/AdminPages/UserManagement/UserAdd.vue
@@ -6,7 +6,7 @@
 
         <form>
             <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4 w-full mt-6 ">
-                <div class="w-full ">
+                <!-- <div class="w-full ">
                     <span class="p-float-label">
                         <InputText id="firstName" v-model="value" class="w-full"/>
                         <label for="firstName">First name</label>
@@ -36,13 +36,17 @@
                         <label for="phoneNumber">Phone number</label>
                     </span>
                 </div>
-                
+                 -->
                 <!--Complete Address-->
                 
                 <div >
-                    <Dropdown  v-model="selectedProvince" :options="provinces.data" optionLabel="name" placeholder="Select a Province" class="w-full md:w-14rem" />
+                    <Dropdown  v-model="selectedProvince" :options="provinces" optionLabel="name" placeholder="Select a Province" class="w-full md:w-14rem" />
                 </div>
 
+                <div >
+                    <Dropdown  v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City" class="w-full md:w-14rem" />
+                </div>
+                
                 
             </div>
             
@@ -56,19 +60,44 @@
 <script setup>
 import DashboardLayout from '../../Layout/DashboardLayout.vue';
 import axios from 'axios';
-import {ref, onMounted} from 'vue'
+import {ref, onMounted, watch, computed} from 'vue'
 
 
 const provincesUrl =  'https://ph-locations-api.buonzz.com/v1/provinces'
+const citiesUrl = 'https://ph-locations-api.buonzz.com/v1/cities'
+
 const provinces = ref({})
+const cities = ref({})
 
 
 const selectedProvince = ref('')
+const selectedCity = ref('')
+
+
+
+
+ const getCities = async () =>{
+        try{
+            const res = await axios.get(citiesUrl)
+            const tests = res.data.data
+            console.log('my data: '+ res.data.data[1].name)
+            cities.value = res.data.data.filter((val) => val.province_code === selectedProvince.value.id )
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+watch(selectedProvince,(val)=>{
+    getCities()
+})
+
+
+
 
 onMounted(async () => {
   try {
     const response = await axios.get(provincesUrl)
-    provinces.value = response.data
+    provinces.value = response.data.data
   } catch (error) {
     console.error(error)
   }
